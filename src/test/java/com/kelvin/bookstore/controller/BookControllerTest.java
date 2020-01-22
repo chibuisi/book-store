549b45b8fd4130837b3964b0f0e19f37aa59d543
chibuisi <amiaka.chibuisi@gmail.com>
2020-01-22 12:14:29 -0600

    created test for book controller

---
+++ src/test/java/com/kelvin/bookstore/controller/BookControllerTest.java
@@ -0,0 +1,34 @@
+package com.kelvin.bookstore.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getAllBooks() {
+    }
+
+    @Test
+    void getBookByName() {
+    }
+
+    @Test
+    void getBookById() {
+    }
+
+    @Test
+    void saveBook() {
+    }
+}
